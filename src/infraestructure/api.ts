048eda7c5fcdfa4d465dae9e88a7742ec36026c1
franklinnot <franklinnot@outlook.com>
2025-12-02 23:39:26 -0500

    new: - instalacion de axios - creacion de objecto api que hara uso de axios

---
+++ src/infraestructure/api.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.API_URL;
+
+const api = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use(
+  (request) => {
+    console.log(
+      `[API] Solicitud - ${request.method} a: ${request.url}`,
+      request.data
+    );
+    return request;
+  },
+  (error) => {
+    console.error("[API] Error en la solicitud", error);
+    return Promise.reject(error);
+  }
+);
+
+// interceptor para imprimir respuestas y errores globalmente
+api.interceptors.response.use(
+  (response) => {
+    console.log("[API] Respuesta:", response.data);
+    return response;
+  },
+  (error) => {
+    console.log("[API] Error en la respuesta:", error);
+    return Promise.reject(error);
+  }
+);
+
+export { api };
