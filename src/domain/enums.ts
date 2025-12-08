4d8f7f17a25cffb190c267309d8478ab1de14d0a
franklinnot <franklinnot@outlook.com>
2025-12-08 10:16:14 -0500

    new: - enums: AudioQuality, VideoQuality, AudioPlatforms, VideoPlatforms

---
+++ src/domain/enums.ts
@@ -0,0 +1,26 @@
+export enum AudioQuality {
+  LOW = "128",
+  MEDIUM = "192",
+  HIGH = "256",
+  VERY_HIGH = "320",
+}
+
+export enum VideoQuality {
+  LOW = "480",
+  MEDIUM = "720",
+  HIGH = "1080",
+  VERY_HIGH = "1440",
+}
+
+export enum AudioPlatforms {
+  YOUTUBE = "youtube",
+  SOUNDCLOUD = "soundcloud",
+}
+
+export enum VideoPlatforms {
+  YOUTUBE = "youtube",
+  FACEBOOK = "facebook",
+  INSTAGRAM = "instagram",
+  TWITTER = "twitter",
+  PINTEREST = "pinterest",
+}
