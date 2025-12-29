790e578760a4dc809f92d070e5e96262d19a5320
Break <franklinbacacampos@gmail.com>
2025-12-28 23:15:32 -0500

    nuevo componente input, para evitar duplicar codigo. Estilos base con opcion de ser modificados

---
+++ src/presentation/components/button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { mixStyle } from "../utils/mix-style";
+import type { ButtonProps } from "../utils/html-props";
+
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <button
+        className={mixStyle(
+          `size-full bg-white text-(--dark-primary) p-2
+          focus:bg-emerald-400 active:bg-emerald-500 rounded-md 
+          focus:outline-2 focus:outline-solid focus:outline-(--light-secondary)`,
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
