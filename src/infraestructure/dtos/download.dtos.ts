acadc8a30cd32f9585c38da77a94f1383f8e9344
Break <franklinbacacampos@gmail.com>
2026-01-01 22:03:58 -0500

    simplificacion de dtos

--- src/infraestructure/dtos/download.dtos.ts
+++ src/infraestructure/dtos/download.dtos.ts
@@ -1,13 +1,8 @@
 import { z } from "zod";
-import {
-  AudioPlatforms,
-  AudioQuality,
-  VideoPlatforms,
-  VideoQuality,
-} from "../../domain/enums";
+import { Platforms } from "../../domain/enums";
 
 // Esquemas
-export const Schema_AudioDownload = z.object({
+export const Schema_Download = z.object({
   url: z
     .url("Ingresa una url válida.")
     .max(2048, "La url debe tener menos de 2048 caracteres."),
@@ -15,25 +10,11 @@ export const Schema_AudioDownload = z.object({
     .string()
     .max(64, "El título debe tener menos de 64 caracteres.")
     .optional(),
-  platform: z.enum(AudioPlatforms).default(AudioPlatforms.YOUTUBE),
-  quality: z.enum(AudioQuality).default(AudioQuality.MEDIUM),
-});
-
-export const Schema_VideoDownload = z.object({
-  url: z
-    .url("Ingresa una url válida.")
-    .max(2048, "La url debe tener menos de 2048 caracteres."),
-  title: z
-    .string()
-    .max(64, "El título debe tener menos de 64 caracteres.")
-    .optional(),
-  platform: z.enum(VideoPlatforms).default(VideoPlatforms.YOUTUBE),
-  quality: z.enum(VideoQuality).default(VideoQuality.MEDIUM),
+  platform: z.enum(Platforms).default(Platforms.YOUTUBE),
 });
 
 // DTOS
-export type DTO_AudioDownload = z.infer<typeof Schema_AudioDownload>;
-export type DTO_VideoDownload = z.infer<typeof Schema_VideoDownload>;
+export type DTO_Download = z.infer<typeof Schema_Download>;
 // DTO de response
 export interface RES_GetIframe {
   url: string;
--- src/infraestructure/dtos/get_iframe.dtos.ts
+++ src/infraestructure/dtos/get_iframe.dtos.ts
@@ -1,21 +1,13 @@
 import { z } from "zod";
-import { AudioPlatforms, VideoPlatforms } from "../../domain/enums";
+import { Platforms } from "../../domain/enums";
 
 // Esquemas
-export const Schema_GetAudioIframe = z.object({
+export const Schema_GetIframe = z.object({
   url: z
     .url("Ingresa una url válida.")
     .max(2048, "La url debe tener menos de 2048 caracteres."),
-  platform: z.enum(AudioPlatforms).default(AudioPlatforms.YOUTUBE),
-});
-
-export const Schema_GetVideoIframe = z.object({
-  url: z
-    .url("Ingresa una url válida.")
-    .max(2048, "La url debe tener menos de 2048 caracteres."),
-  platform: z.enum(VideoPlatforms).default(VideoPlatforms.YOUTUBE),
+  platform: z.enum(Platforms).default(Platforms.YOUTUBE),
 });
 
 // DTOS
-export type DTO_GetAudioIframe = z.infer<typeof Schema_GetAudioIframe>;
-export type DTO_GetVideoIframe = z.infer<typeof Schema_GetVideoIframe>;
+export type DTO_GetIframe = z.infer<typeof Schema_GetIframe>;
