19b6005c53a9af81ddcc7d10758e9c626f835a91
Break <franklinbacacampos@gmail.com>
2026-01-01 22:18:42 -0500

    uso de enums simplificados en base al backend

--- src/domain/enums.ts
+++ src/domain/enums.ts
@@ -1,26 +1,4 @@
-export enum AudioQuality {
-  LOW = "128",
-  MEDIUM = "192",
-  HIGH = "256",
-  VERY_HIGH = "320",
-}
-
-export enum VideoQuality {
-  LOW = "480",
-  MEDIUM = "720",
-  HIGH = "1080",
-  VERY_HIGH = "1440",
-}
-
-export enum AudioPlatforms {
+export enum Platforms {
   YOUTUBE = "youtube",
   SOUNDCLOUD = "soundcloud",
-}
-
-export enum VideoPlatforms {
-  YOUTUBE = "youtube",
-  FACEBOOK = "facebook",
-  INSTAGRAM = "instagram",
-  TWITTER = "twitter",
-  PINTEREST = "pinterest",
 }
