3f016cdba423a1e74b56208f09bfcda4ae216f49
Break <franklinbacacampos@gmail.com>
2026-01-04 19:23:32 -0500

    simplificacion del componente de loading

--- src/presentation/components/loading-animated.tsx
+++ src/presentation/components/loading-animated.tsx
@@ -2,37 +2,26 @@ import { Isotipo } from "./isotipo";
 import { useEffect, useState } from "react";
 
 export default function LoadingAnimated({ isLoading }: { isLoading: boolean }) {
-  const [shouldRender, setShouldRender] = useState(isLoading);
-  const [isFadingOut, setIsFadingOut] = useState(false);
+  const [display, setDisplay] = useState(isLoading);
 
   useEffect(() => {
-    let timer: ReturnType<typeof setTimeout>;
-
     if (isLoading) {
-      setShouldRender(true);
-      setIsFadingOut(false);
+      setDisplay(true);
     } else {
-      if (shouldRender) {
-        setIsFadingOut(true);
-        timer = setTimeout(() => {
-          setShouldRender(false);
-        }, 300);
-      }
+      const timer = setTimeout(() => setDisplay(false), 300);
+      return () => clearTimeout(timer);
     }
-    return () => clearTimeout(timer);
-  }, [isLoading, shouldRender]);
+  }, [isLoading]);
 
-  if (!shouldRender) {
-    return null;
-  }
+  if (!display && !isLoading) return null;
 
   return (
     <div
-      className={`absolute w-dvw h-dvh grid place-items-center ${
-        isFadingOut ? "animate-fade-out" : "animate-fade-in"
+      className={`absolute inset-0 grid place-items-center ${
+        !isLoading ? "animate-fade-out" : "animate-fade-in"
       }`}
     >
-      <div className="absolute w-dvw h-dvh bg-gray-950 opacity-96" />
+      <div className="absolute inset-0 bg-gray-950/96" />
       <Isotipo className="w-[68dvw] animate-pulse-glow mb-[12dvh] z-50" />
     </div>
   );
