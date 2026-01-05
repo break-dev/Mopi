e2fe6c5765e127721330707629f91c0033f0fc34
Break <franklinbacacampos@gmail.com>
2026-01-04 19:25:00 -0500

    evitar recibir la plataforma especificada por el usuario para obtener el iframe

---
+++ src/application/hooks/useGetIframe.hook.ts
@@ -0,0 +1,53 @@
+import { Schema_GetIframe } from "../../infraestructure/dtos/get_iframe.dtos";
+import { GetIframe } from "../../infraestructure/requests/get_iframe";
+import type { ErrorResponse } from "../../infraestructure/response";
+import { platformAnalyzer } from "../utils/platformAnalyzer";
+
+interface UseGetIframeProps {
+  url: string;
+  setIsLoading: (isLoading: boolean) => void;
+  setError: (error: ErrorResponse) => void;
+  setIframe: (iframe: string) => void;
+}
+
+export const useGetIframe = ({
+  url,
+  setIsLoading,
+  setError,
+  setIframe,
+}: UseGetIframeProps) => {
+  const get_iframe = async (): Promise<void> => {
+    const platform = platformAnalyzer(url);
+    if (!platform) {
+      setError("Plataforma no soportada");
+      return;
+    }
+
+    setIsLoading(true);
+    setError(null);
+    let error: ErrorResponse = null;
+    let result: string = "";
+
+    const schema = Schema_GetIframe.safeParse({ url, platform });
+    if (schema.success) {
+      try {
+        const response = await GetIframe.get_iframe(schema.data);
+        if (response.success) {
+          result = response.data?.url ?? "";
+          error = !result ? "Ups! Hubo un error..." : null;
+        } else {
+          error = response.error;
+        }
+      } catch {
+        error = "Ups! Hubo un error...";
+      }
+    } else {
+      error = schema.error.issues.map((i) => i.message);
+    }
+    setIsLoading(false);
+    setError(error);
+    setIframe(result);
+  };
+
+  return { get_iframe };
+};
